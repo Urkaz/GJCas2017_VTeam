a25d2a6700a079b33c2230d3c20783ea47fb9d97
drain111 <adriacs23@gmail.com>
2017-02-11 13:25:19 +0100

    Basic mechanics

---
+++ Assets/MarioYAdriScripts/PickUpType.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpType : MonoBehaviour {
+
+    public Inventory.Items type;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+    }
+
+
+}
