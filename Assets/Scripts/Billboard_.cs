e79825af996f570acc3258b2f2a456d73cdd1dd6
Urkaz <mrurkaz@gmail.com>
2017-02-11 18:42:02 +0100

    Billboard

--- Assets/Scripts/Billboard_.cs
+++ Assets/Scripts/Billboard_.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Billboard : MonoBehaviour {
+public class Billboard_ : MonoBehaviour {
     public Camera m_Camera;
 
     void Update()
