65cf00f478ca0df24e6f405d132c0e400423dbe8
Urkaz <mrurkaz@gmail.com>
2017-02-12 22:46:23 +0100

    Controller

--- Assets/Scripts/ControllerIcon.cs
+++ Assets/Scripts/ControllerIcon.cs
@@ -11,21 +11,23 @@ public class ControllerIcon : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		string[] c = Input.GetJoystickNames();
-		if(c[0].Equals("Xbox Bluetooth Gamepad")) {
-			Image i = GetComponent<Image>();
-			if(i != null)
-				i.sprite = xbo;
-			SpriteRenderer s = GetComponent<SpriteRenderer>();
-			if(s != null)
-				s.sprite = xbo;
-		}	
-		else {
-			Image i = GetComponent<Image>();
-			if(i != null)
-				i.sprite = pc;
-			SpriteRenderer s = GetComponent<SpriteRenderer>();
-			if(s != null)
-				s.sprite = pc;
+		if(c.Length > 0) {
+			if(c[0].Equals("Xbox Bluetooth Gamepad")) {
+				Image i = GetComponent<Image>();
+				if(i != null)
+					i.sprite = xbo;
+				SpriteRenderer s = GetComponent<SpriteRenderer>();
+				if(s != null)
+					s.sprite = xbo;
+			}	
+			else {
+				Image i = GetComponent<Image>();
+				if(i != null)
+					i.sprite = pc;
+				SpriteRenderer s = GetComponent<SpriteRenderer>();
+				if(s != null)
+					s.sprite = pc;
+			}
 		}
 	}
 }
