7d97137c6bfccae02d37673253fd5f59ac2dc2ac
drain111 <adriacs23@gmail.com>
2017-02-12 15:54:19 +0100

    Add audio

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -26,6 +26,8 @@ public class PenguinController: MonoBehaviour {
 
     private GameManager gm;
 
+    private GameObject currentDoorInteracted;
+
     public enum State
     {
         ALIVE,
@@ -67,6 +69,17 @@ public class PenguinController: MonoBehaviour {
         if (Input.GetAxis("ResetRoom") != 0 && currentState != State.DEAD)
         {
             Dead();
+        }
+
+        if ( currentDoorInteracted != null && !currentDoorInteracted.gameObject.GetComponent<AudioSource>().isPlaying)
+        {
+            if (gm != null)
+            {
+                int[] a = currentDoorInteracted.GetComponent<SwitchScene>().GetTargetDoor(); //0 = nivel , 1 = puerta
+                gm.setSpawnTarget(a[1]);
+                gm.LoadLevel((GameManager.Levels)a[0]);
+                currentDoorInteracted = null;
+            }
         }
     }
 
@@ -159,7 +172,7 @@ public class PenguinController: MonoBehaviour {
                 {
                     if((int) currentCollidedObject.GetComponent<PickUpType>().type >= (int) Inventory.Items.Pipe1 )
                     {
-                        if( !(inv.Contains(Inventory.Items.Pipe1) || inv.Contains(Inventory.Items.Pipe2) || inv.Contains(Inventory.Items.Pipe2)))
+                        if( !(inv.Contains(Inventory.Items.Pipe1) || inv.Contains(Inventory.Items.Pipe2) || inv.Contains(Inventory.Items.Pipe3)))
                         {
                             inv.addObject((currentCollidedObject.GetComponent<PickUpType>().type));
                             Destroy(currentCollidedObject);
@@ -182,13 +195,17 @@ public class PenguinController: MonoBehaviour {
             if (!door.GetComponent<SwitchScene>().neededItem.Equals(Inventory.Items.NONE))
                 inv.removeObject(door.GetComponent<SwitchScene>().neededItem);
 
-            int[] a = door.GetComponent<SwitchScene>().GetTargetDoor(); //0 = nivel , 1 = puerta
+            //int[] a = door.GetComponent<SwitchScene>().GetTargetDoor(); //0 = nivel , 1 = puerta
 
-            if (gm != null)
+            currentDoorInteracted = door;
+            door.gameObject.GetComponent<AudioSource>().Play();
+            
+            /*if (gm != null)
             {
+
                 gm.setSpawnTarget(a[1]);
                 gm.LoadLevel((GameManager.Levels)a[0]);
-            }
+            }*/
         }
         
 
